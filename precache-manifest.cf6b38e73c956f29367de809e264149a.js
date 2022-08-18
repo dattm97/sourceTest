31351f6ce252427a2341e55738cc498b90f9cba3
dattm <dattm@edoctor.vn>
2022-08-18 15:26:28 +0700

    first commit

---
+++ precache-manifest.cf6b38e73c956f29367de809e264149a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb37610c2aede78597eb7db14e7b7f5e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7a800e0a2fa2ad2b5f8",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "b13cd9390ae2e6ff36d4",
+    "url": "/static/js/2.324c27da.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/static/js/2.324c27da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7a800e0a2fa2ad2b5f8",
+    "url": "/static/js/main.998bab39.chunk.js"
+  },
+  {
+    "revision": "54ff6f61acf9b1d227fb",
+    "url": "/static/js/runtime-main.35769ded.js"
+  }
+]);
